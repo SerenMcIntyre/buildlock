8dd53d65f30649e912b12eb16f9ec84dd8ec6690
SerenMcIntyre <serenmcintyre@gmail.com>
2024-11-09 01:02:26 +0000

    Added Skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,9 @@
 	import '../app.css';
 	import { treaty } from '@elysiajs/eden';
 	import { PUBLIC_BASE_URL } from '$env/static/public';
+	import { Nav } from '@skeletonlabs/skeleton-svelte';
+	import { Bolt, Bookmark, CircleUserRound, Swords } from 'lucide-svelte/icons';
+	import Github from '$lib/assets/brand-icons/Github.svelte';
 	let { children } = $props();
 
 	const client = treaty<App>(PUBLIC_BASE_URL);
@@ -12,8 +15,36 @@
 	$effect(() => {
 		client.api.health.get().then((res) => console.log(res.data));
 	});
+
 </script>
 
 <ParaglideJS {i18n}>
-	{@render children()}
+	<div class="card border-surface-100-900 grid h-screen w-full grid-cols-[1fr_auto] border-[1px]">
+		<div class="flex items-center justify-center">
+			{@render children()}
+		</div>
+		<Nav.Rail width="w-16"  classes="transition-all">
+			{#snippet header()}
+				<Nav.Tile labelExpanded="GitHub" href="https://github.com/SerenMcIntyre/buildlock" target="_blank">
+					<Github classes="w-6 h-6 fill-primary-contrast-500" />
+				</Nav.Tile>
+			{/snippet}
+			{#snippet tiles()}
+				<Nav.Tile id="0"  label="Build" href="#">
+					<Bolt class="stroke-surface-300" />
+				</Nav.Tile>
+				<Nav.Tile id="1" label="Saved" href="#">
+					<Bookmark class="stroke-surface-300" />
+				</Nav.Tile>
+				<Nav.Tile id="2" label="Items" href="#">
+					<Swords class="stroke-surface-300" />
+				</Nav.Tile>
+				{/snippet}
+			{#snippet footer()}
+				<button type="button" class="btn w-full flex justify-start">
+					<CircleUserRound class="stroke-surface-300" />
+				</button>
+			{/snippet}
+		</Nav.Rail>
+	</div>
 </ParaglideJS>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,12 +4,27 @@ import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
+import { join } from 'path';
+import { skeleton } from '@skeletonlabs/skeleton/plugin';
+import * as themes from '@skeletonlabs/skeleton/themes';
+
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton-svelte'), '../**/*.{html,js,svelte,ts}')
+	],
 
 	theme: {
 		extend: {}
 	},
 
-	plugins: [typography, forms, containerQueries, aspectRatio]
+	plugins: [
+		typography,
+		forms,
+		containerQueries,
+		aspectRatio,
+		skeleton({
+			themes: [themes.catppuccin]
+		})
+	]
 } satisfies Config;
